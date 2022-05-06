4bd82dec2c090d20fca671f38f94a29c6885d5f0
Alexander Danilov <h4rb4rd@gmail.com>
2022-05-06 20:50:37 +0300

    feat: 404 page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import img from '../../assets/warning.svg';
 
 import cl from './NotFound.module.scss';
 
 const NotFound = () => {
-  return <div>404</div>;
+  return (
+    <div className={cl.error}>
+      <img src={img} alt="warning" />
+      <h2>Oops! 404 - PAGE NOT FOUND</h2>
+      <p>The page you are looking for might have been removed</p>
+      <p>had its name changed or temporarily unavailable.</p>
+      <Link to="/">Go to homepage</Link>
+    </div>
+  );
 };
 
 export default NotFound;
