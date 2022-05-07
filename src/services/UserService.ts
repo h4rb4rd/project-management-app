66930ff576f030c3843fea74e6ff9180e52b8b6e
Alexander Danilov <h4rb4rd@gmail.com>
2022-05-07 20:38:41 +0300

    refactor: footer & team small improvements

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -11,4 +11,7 @@ export default class UserService {
   static async getUser(id: string): Promise<AxiosResponse<IUser>> {
     return $api.get<IUser>(`/users/${id}`);
   }
+  static async deleteUser(id: string) {
+    $api.delete(`/users/${id}`);
+  }
 }
